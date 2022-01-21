68778beeb9749ad104657d30107050a1033e8e42
Raul Mihaila <raulmihaila@Administrators-MacBook-Pro.local>
2022-01-21 09:27:03 +0200

    small refactoring

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
+const baseUrl = 'https://deckofcardsapi.com/api/deck/';
+
 export async function get(path: string) {
-    return (await fetch(`https://deckofcardsapi.com/api/deck/${path}`)).json();
+    return (await fetch(`${baseUrl}${path}`)).json();
 }
 
 export async function post(path: string, data?: Object) {
@@ -11,5 +13,5 @@ export async function post(path: string, data?: Object) {
         Object.entries(data).forEach(([key, value]) => body!.append(key, value));
     }
 
-    return (await fetch(`https://deckofcardsapi.com/api/deck/${path}`, {method: 'post', body})).json();
+    return (await fetch(`${baseUrl}${path}`, {method: 'post', body})).json();
 }
